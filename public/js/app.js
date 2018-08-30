557461e41da6057796f6f40e0ceb8d27e3c96857
Eric Bidelman <ebidel@gmail.com>
2018-08-29 17:28:26 -0700

    Adding sharing to posts

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,6 @@
 /* global ga:false */
 
 const scroller = document.scrollingElement || document.body;
-// const navbar = document.querySelector('.navbar');
-
-// let _disableElementScrollTimeout = null;
 
 // From http://en.wikipedia.org/wiki/Smoothstep
 // eslint-disable-next-line
@@ -57,60 +54,18 @@ function smoothScroll(el, duration = 1, callback = null, offset = 0) {
   cb(startTime);
 }
 
-// /**
-//  * Disables a container from user interaction (using pointer-events: none)
-//  * when the page is scrolling. Enables it again
-//  *
-//  * @param {HTMLElement} container The container to
-//  * @param {Function} callback Optional callback, invoked when the user
-//  *     has stopped scrolling after opt_enableAfter ms (or default 250ms).
-//  * @param {boolean} enableAfter Optional number of ms to enable
-//  *     interaction on the container after the user has stopped scrolling.
-//  */
-// function disableElementOnScroll(container, callback=null, enableAfter=250) {
-//   if (!_disableElementScrollTimeout) {
-//     container.style.pointerEvents = 'none';
-//   }
-
-//   clearTimeout(_disableElementScrollTimeout);
-
-//   _disableElementScrollTimeout = setTimeout(function() {
-//     container.style.pointerEvents = 'auto';
-//     callback && callback();
-//     _disableElementScrollTimeout = null;
-//   }, enableAfter);
-// }
-
-// window.addEventListener('scroll', function(e) {
-//   navbar.classList.toggle('colorize', scroller.scrollTop > 0);
-
-//   // Call page's scroll handler if it defines one.
-//   exports.onScroll && exports.onScroll(scroller.scrollTop);
-// });
-
 const fab = document.querySelector('.fab');
 fab.addEventListener('click', e => {
   smoothScroll(document.body, 1);
 });
 
-// navbar.addEventListener('click', e => {
-//   if (window.matchMedia('(max-width: 767px)').matches) {
-//     navbar.classList.toggle('open');
-//   }
-// });
-
-// document.addEventListener('click', e => {
-//   if (window.matchMedia('(max-width: 767px)').matches &&
-//       navbar.classList.contains('open') && !navbar.contains(e.target)) {
-//     navbar.classList.remove('open');
-//   }
-// });
-
 /**
  * Share the current page via navigator.share (if available) or Twitter
  * as fallback.
  */
-async function share() {
+async function share(e) {
+  e.preventDefault();
+
   const url = location.href || '';
   const title = document.title || '';
 
@@ -128,8 +83,9 @@ async function share() {
   }
 }
 
-export {
-  smoothScroll,
-  share,
-  // disableElementOnScroll,
-};
+window.share = share;
+
+// export {
+//   smoothScroll,
+//   share,
+// };
